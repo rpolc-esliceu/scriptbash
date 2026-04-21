05fac37d1f6f74bc43a19f1c2e6e3e9123266654
ramon <rpolc@esliceu.net>
2026-04-21 17:11:41 +0200

    hem comprovat que erros usage de crear usuaris

--- crear_user.sh
+++ crear_user.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
+function usage(){
+    echo "Usage ${0}"
+    echo
+    echo "has de ser root"
+    echo "l'usari no és pot reptir"
+    exit 1
+}
+
 
 #Script per crear un usari
 
 #primer comprovam si som l'usari root
 if [[ ${UID} -ne 0 ]]
 then
-    echo "Usage ${0}"
-    echo
-    echo "has de ser root"
-    exit 1
+    usage
 fi
 
 read -p "Introduix el d'usuari: " USER_NAME
+
+
 
 echo "creant l'usauri ${USER_NAME}"
